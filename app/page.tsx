aab428642a4d3877db6b4291aa20e7e5a8e95309
Пользователь <vitaliyprudnyk@gmail.com>
2025-05-28 16:52:37 +0300

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
 import ResentProjects from "@/components/ResentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
